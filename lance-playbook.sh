4cf2c6417e9b321c3f6d77be92ad6d6232cd4b72
Christophe Mougin <christophe.mougin@laposte.net>
2024-09-18 19:21:06 +0200

    comment in shell

--- lance-playbook.sh
+++ lance-playbook.sh
@@ -1,3 +1,4 @@
+# lancer à partir de wsl (plutôt kali que ubuntu)
  export ANSIBLE_CONFIG=./ansible.cfg
  # ansible-playbook --extra-vars="ansible_become_pass=xl......" ./rclone+borgmatic-intoDocker.yml
  ansible-playbook -b ./rclone+borgmatic-intoDocker.yml
