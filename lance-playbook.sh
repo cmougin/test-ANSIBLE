8cc9254be5fb6fd3119ecf073891df28510fd655
Christophe Mougin <christophe.mougin@worldline.com>
2024-10-08 15:53:45 +0200

    passage en quotidien à 1h00 du matin

--- lance-playbook.sh
+++ lance-playbook.sh
@@ -2,3 +2,5 @@ export ANSIBLE_CONFIG=./ansible.cfg
 # ansible-playbook --extra-vars="ansible_become_pass=xl......" ./rclone+borgmatic-intoDocker.yml
 ansible-playbook -b ./rclone+borgmatic-intoDocker.yml
 # lancer à partir de wsl (plutôt kali que ubuntu)
+
+## TODO: pour une raison qui m’échappe, ce shell ne marche pas quand on l’appelle, mais si on tape les commandes qu’il contient dans un shell WSL, ça marche
